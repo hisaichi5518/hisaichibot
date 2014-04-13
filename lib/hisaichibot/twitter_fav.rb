5a7b32054dc7fb0f77b2ed50250414781a48c53a
hisaichi5518 <hisaichi5518@gmail.com>
2014-04-13 19:26:45 +0900

    (๑╹ڡ╹๑)twitter fav

---
+++ lib/hisaichibot/twitter_fav.rb
@@ -0,0 +1,19 @@
+require 'tweetstream'
+
+module Hisaichibot
+  class TwitterFav
+    attr_accessor :message
+    def initialize(m)
+      @message = m
+    end
+
+    def execute
+      client = TweetStream::Client.new
+      client.on(:favorite) do |event|
+        message.reply "#{event[:source][:name]} faved: #{event[:target_object][:text]} #{event[:target_object][:user][:screen_name]}"
+      end
+
+      client.userstream
+    end
+  end
+end
